f75836a4b1afdeedab41ee2c0bc4235a3a7dbfc9
Blend06 <bk67418@ubt-uni.net>
2025-04-17 21:28:56 +0200

    stili rregullum

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'; // Make sure to import from 'react-dom/
 import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 
 import App from './App'; // Import App component
+import './assets/scss/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Create root element
 
