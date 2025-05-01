fe6f04f4149d037538e96ded71de2423a32a4d93
AidAliu <aidaliu50@gmail.com>
2025-05-02 01:41:21 +0200

    Fixed registers bug

--- backend/api/serializers/user_serializer.py
+++ backend/api/serializers/user_serializer.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from api.models import User
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['Userid', 'username', 'email', 'password', 'is_active', 'is_staff']
+        read_only_fields = ['Userid', 'is_active', 'is_staff']
+
+    def create(self, validated_data):
+        pwd = validated_data.pop('password')
+        #this will call CustomUserManager.create_user to hash te password and save:
+        user = User.objects.create_user(**validated_data, password=pwd)
+        return user
